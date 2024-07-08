ad5fe3fcc3046eff5b4ae32619e70847bed7e1e7
foxholic9 <roy0915z@naver.com>
2024-07-08 14:01:00 +0900

    Feat: User관련 api함수 추가 완료

--- app/ddd/Test2.tsx
+++ app/ddd/Test2.tsx
@@ -1,21 +1,53 @@
 'use client';
 
-import { useGetMyInfoQuery, useUpdateMyInfo, useGetUserInfoQuery } from '@/hooks/User';
-import { useDataQuery } from '@/services/common';
+import {
+  useGetMyInfoQuery,
+  useUpdateMyInfo,
+  useGetUserInfoQuery,
+  useGetFolloewers,
+  useGetFolloewees,
+  useGetItemsFavoriteProducts,
+  useGetUserRanking,
+} from '@/hooks/User';
 
 function MyInfo() {
   const { data: myInfoData, isLoading, error } = useGetMyInfoQuery({ url: '/users/270' });
   const updateMyData = useUpdateMyInfo();
   const { data: userInfoData } = useGetUserInfoQuery({ userId: 268 });
-  const handleButtonClick = () => {
+  const { data: followersData } = useGetFolloewers();
+  const { data: followeesData } = useGetFolloewees();
+  const { data: favoriteProductsData } = useGetItemsFavoriteProducts();
+  const { data: userRankingData } = useGetUserRanking({ params: { limit: 5 } });
+
+  const handleButtonClick1 = () => {
     updateMyData({ nickname: 'james2' });
+  };
+  const handleButtonClick2 = () => {
+    console.log('Followers:', followersData);
+  };
+  const handleButtonClick3 = () => {
+    console.log('Followees:', followeesData);
+  };
+  const handleButtonClick4 = () => {
+    console.log('Favorite Products:', favoriteProductsData);
+  };
+  const handleButtonClick5 = () => {
+    console.log('User Ranking:', userRankingData);
   };
 
   return (
     <>
       <div>{myInfoData?.id}</div>
       <div>{userInfoData?.id}</div>
-      <button onClick={handleButtonClick}>닉변</button>
+      <button onClick={handleButtonClick1}>닉변</button>
+      <br />
+      <button onClick={handleButtonClick2}>팔로워</button>
+      <br />
+      <button onClick={handleButtonClick3}>팔로위</button>
+      <br />
+      <button onClick={handleButtonClick4}>찜목록</button>
+      <br />
+      <button onClick={handleButtonClick5}>랭킹</button>
     </>
   );
 }
--- hooks/User.ts
+++ hooks/User.ts
@@ -1,59 +1,150 @@
+import { url } from 'inspector';
 import { useDataMutation, useDataQuery } from '../services/common';
-import { UpdateUserRequest, UserResponse, HttpMethod } from '../types/data';
+import {
+  UpdateUserRequest,
+  UserResponse,
+  HttpMethod,
+  ProductsListResponse,
+  RankedUserResponse,
+  FollowersResponse,
+} from '../types/data';
 
-interface GetMyInfoConfig {
+interface BaseRequestConfig {
   url?: string;
   options?: object;
+  params?: object;
 }
 
-interface UpdateMyInfoConfig {
+interface UpdateMyInfoConfig extends BaseRequestConfig {
   method?: HttpMethod;
-  Url?: string;
-  options?: object;
 }
 
-interface GetUserInfoConfig {
-  url?: string;
-  options?: object;
+interface GetUserInfoConfig extends BaseRequestConfig {
   userId: number;
 }
 
+interface GetListInfoConfig extends BaseRequestConfig {
+  userId: number;
+  cursor?: number;
+}
+
 //내 정보 가져오기. url 미입력시 자동으로 로그인 된 유저의 정보를 가져옴
-export const useGetMyInfoQuery = (config: GetMyInfoConfig = {}) => {
-  const { url = '/users/me', options = {} } = config;
+export const useGetMyInfoQuery = (config: BaseRequestConfig = {}) => {
+  const { url = '/users/me', options = {}, params } = config;
   const query = useDataQuery<UserResponse>({
     queryKey: ['userInfo', 'me'], //로그아웃시 stale하게 만들어야됨 혹은 로그인 된 유저의 id 필요
     url,
-    options: {
-      ...options,
-    },
+    options,
+    params,
   });
   return query;
 };
 
 //내 정보 수정하기.
 export const useUpdateMyInfo = (config: UpdateMyInfoConfig = {}) => {
-  const { method = 'PATCH', Url = '/auth/signUp', options = {} } = config;
+  const { method = 'PATCH', url = '/auth/signUp', options = {} } = config;
 
-  const mutation = useDataMutation<UpdateUserRequest, UserResponse>();
+  const mutation = useDataMutation<UpdateUserRequest, UserResponse>(options);
 
-  return (body: UpdateUserRequest, urlParam?: string | number) => {
-    const url = urlParam ? `${Url}/${urlParam}` : Url;
+  return (body: UpdateUserRequest) => {
     return mutation.mutate({ url, method, data: body });
   };
 };
 
 //다른 유저 정보 가져오기. 유저 id 필요
 export const useGetUserInfoQuery = (config: GetUserInfoConfig = { userId: 0 }) => {
-  const { url = `/users`, options = {}, userId } = config;
-  const fullUrl = `${url}/${userId}`;
+  const { url = `/users`, options = {}, userId, params } = config;
+  const fullUrl = userId ? `${url}/${userId}` : url;
 
   const query = useDataQuery<UserResponse>({
     queryKey: ['userInfo', userId],
     url: fullUrl,
-    options: {
-      ...options,
-    },
+    options,
+    params,
+  });
+  return query;
+};
+
+//랭킹유저 목록 조회
+export const useGetUserRanking = (config: BaseRequestConfig = {}) => {
+  const { url = '/users/ranking', options = {}, params } = config;
+
+  const query = useDataQuery<RankedUserResponse[]>({
+    queryKey: ['userInfo', 'ranking'],
+    url,
+    options,
+    params,
+  });
+
+  return query;
+};
+
+//userID 유저가 생성한 상품 조회
+export const useGetItemsCreatedProducts = (config: GetListInfoConfig = { userId: 0 }) => {
+  const { url = '/users', options = {}, userId, params } = config;
+  const fullUrl = userId ? `${url}/${userId}/created-products` : url;
+
+  const query = useDataQuery<ProductsListResponse>({
+    queryKey: ['ProductsList', userId, 'created'],
+    url: fullUrl,
+    options,
+    params,
+  });
+  return query;
+};
+
+//userID 유저가 리뷰한 상품 조회
+export const useGetItemsReviewdProducts = (config: GetListInfoConfig = { userId: 0 }) => {
+  const { url = '/users', options = {}, userId, params } = config;
+  const fullUrl = userId ? `${url}/${userId}/reviewed-products` : url;
+
+  const query = useDataQuery<ProductsListResponse>({
+    queryKey: ['ProductsList', userId, 'reviewed'],
+    url: fullUrl,
+    options,
+    params,
+  });
+  return query;
+};
+
+//userID 유저가 찜한 상품 조회
+export const useGetItemsFavoriteProducts = (config: GetListInfoConfig = { userId: 0 }) => {
+  const { url = '/users', options = {}, userId, params } = config;
+  const fullUrl = userId ? `${url}/${userId}/favorite-products` : url;
+
+  const query = useDataQuery<ProductsListResponse>({
+    queryKey: ['ProductsList', userId, 'favorite'],
+    url: fullUrl,
+    options,
+    params,
+  });
+  return query;
+};
+
+//userId 유저가 팔로우한 유저 조회
+export const useGetFolloewees = (config: GetListInfoConfig = { userId: 0 }) => {
+  const { url = '/users', options = {}, userId, params } = config;
+  const fullUrl = userId ? `${url}/${userId}/followees` : url;
+
+  const query = useDataQuery<FollowersResponse>({
+    queryKey: ['followees', userId],
+    url: fullUrl,
+    options,
+    params,
+  });
+  return query;
+};
+
+//userId 유저가 팔로우한 유저 조회
+export const useGetFolloewers = (config: GetListInfoConfig = { userId: 0 }) => {
+  const { url = '/users', options = {}, userId, params } = config;
+  const fullUrl = userId ? `${url}/${userId}/followers` : url;
+
+  const query = useDataQuery<FollowersResponse>({
+    queryKey: ['followers', userId],
+    url: fullUrl,
+    options,
+    params,
   });
   return query;
 };
